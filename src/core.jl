76d32d06fefbba6b5ec4497f30eb9377ebc3939e
Andrei Zhabinski <andrei.zhabinski@gmail.com>
2021-06-12 18:30:31 +0300

    Add ChainRulesCore to dependencies

--- src/core.jl
+++ src/core.jl
@@ -3,6 +3,7 @@ using LinearAlgebra
 # using Setfield
 using OrderedCollections
 using IRTools
+using ChainRulesCore
 using CUDA
 
 
@@ -14,3 +15,13 @@ include("devices.jl")
 include("tape.jl")
 include("trace.jl")
 include("compile.jl")
+
+
+const BEST_AVAILABLE_DEVICE = Ref{AbstractDevice}(CPU())
+best_available_device() = BEST_AVAILABLE_DEVICE[]
+
+function __init__()
+    if CUDA.functional()
+        BEST_AVAILABLE_DEVICE[] = GPU(1)
+    end
+end
