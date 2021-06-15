eccc230110a71e496d3095bd4e521104521c3cc4
Andrei Zhabinski <andrei.zhabinski@gmail.com>
2021-06-15 23:50:26 +0300

    Add doc deployment action

--- docs/make.jl
+++ docs/make.jl
@@ -14,9 +14,6 @@ makedocs(
     ],
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/dfdx/Ghost.jl.git",
+)
